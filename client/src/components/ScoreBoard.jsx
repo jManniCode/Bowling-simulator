456fa419ec2d1fbe26ba41df21cfa4ab155887dc
jManniCode <jonas_manni@hotmail.com>
2025-02-03 23:50:03 +0100

    Deleted some files and started hardcoding the frontend with react

---
+++ client/src/components/ScoreBoard.jsx
@@ -0,0 +1,99 @@
+import "../styles.css";
+
+export default function ScoreBoard() {
+    return (
+        <main>
+            <header>
+                <h1>ScoreBoard</h1>
+            </header>
+            <section>
+
+            <div className="round">
+                    <div className="points" id="round1">
+                        
+                    </div>
+                    
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="round">
+                    <div className="points" id="round1">
+
+                    </div>
+
+                    <div className="points" id="round2">
+
+                    </div>
+            </div>
+            <div className="final-round">
+
+            </div>
+            </section>
+
+        </main>
+    );
+}
