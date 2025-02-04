7562c5fa435e264d8a10c952db9785253cf3320a
jManniCode <jonas_manni@hotmail.com>
2025-02-04 18:57:38 +0100

    Added p tags in Scoreboard
    Added p tags in Scoreboard to hold "-" to simulate that there is no
    score yet

--- client/src/components/ScoreBoard.jsx
+++ client/src/components/ScoreBoard.jsx
@@ -9,58 +9,58 @@ export default function ScoreBoard() {
             <section>
                 <section className="round-section">
                     <div className="round" id="round1">
-                        <div className="points" id="points1-1"></div>
-                        <div className="points" id="points1-2"></div>
+                        <div className="points" id="points1-1"><p>-</p></div>
+                        <div className="points" id="points1-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round2">
-                        <div className="points" id="points2-1"></div>
-                        <div className="points" id="points2-2"></div>
+                        <div className="points" id="points2-1"><p>-</p></div>
+                        <div className="points" id="points2-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round3">
-                        <div className="points" id="points3-1"></div>
-                        <div className="points" id="points3-2"></div>
+                        <div className="points" id="points3-1"><p>-</p></div>
+                        <div className="points" id="points3-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round4">
-                        <div className="points" id="points4-1"></div>
-                        <div className="points" id="points4-2"></div>
+                        <div className="points" id="points4-1"><p>-</p></div>
+                        <div className="points" id="points4-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round5">
-                        <div className="points" id="points5-1"></div>
-                        <div className="points" id="points5-2"></div>
+                        <div className="points" id="points5-1"><p>-</p></div>
+                        <div className="points" id="points5-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round6">
-                        <div className="points" id="points6-1"></div>
-                        <div className="points" id="points6-2"></div>
+                        <div className="points" id="points6-1"><p>-</p></div>
+                        <div className="points" id="points6-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round7">
-                        <div className="points" id="points7-1"></div>
-                        <div className="points" id="points7-2"></div>
+                        <div className="points" id="points7-1"><p>-</p></div>
+                        <div className="points" id="points7-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round8">
-                        <div className="points" id="points8-1"></div>
-                        <div className="points" id="points8-2"></div>
+                        <div className="points" id="points8-1"><p>-</p></div>
+                        <div className="points" id="points8-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round9">
-                        <div className="points" id="points9-1"></div>
-                        <div className="points" id="points9-2"></div>
+                        <div className="points" id="points9-1"><p>-</p></div>
+                        <div className="points" id="points9-2"><p>-</p></div>
                     </div>
                     <div className="round" id="round10">
-                        <div className="points" id="points10-1"></div>
-                        <div className="points" id="points10-2"></div>
-                        <div className="points" id="points10-3"></div>
+                        <div className="points" id="points10-1"><p>-</p></div>
+                        <div className="points" id="points10-2"><p>-</p></div>
+                        <div className="points" id="points10-3"><p>-</p></div>
                     </div>
                 </section>
                 <section className="score-section">
-                    <div className="round-score" id="score1"></div>
-                    <div className="round-score" id="score2"></div>
-                    <div className="round-score" id="score3"></div>
-                    <div className="round-score" id="score4"></div>
-                    <div className="round-score" id="score5"></div>
-                    <div className="round-score" id="score6"></div>
-                    <div className="round-score" id="score7"></div>
-                    <div className="round-score" id="score8"></div>
-                    <div className="round-score" id="score9"></div>
-                    <div className="round-score" id="score10"></div>
+                    <div className="round-score" id="score1"><p>-</p></div>
+                    <div className="round-score" id="score2"><p>-</p></div>
+                    <div className="round-score" id="score3"><p>-</p></div>
+                    <div className="round-score" id="score4"><p>-</p></div>
+                    <div className="round-score" id="score5"><p>-</p></div>
+                    <div className="round-score" id="score6"><p>-</p></div>
+                    <div className="round-score" id="score7"><p>-</p></div>
+                    <div className="round-score" id="score8"><p>-</p></div>
+                    <div className="round-score" id="score9"><p>-</p></div>
+                    <div className="round-score" id="score10"><p>-</p></div>
                 </section>
             </section>
         </main>
